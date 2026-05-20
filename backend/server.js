64279eb96c22d2aacce03b70d9a16e9a7e5d7962
omkar <bhapkaromkar12@gmail.com>
2026-05-20 15:35:37 +0530

    Cloudinary backend setup finalized

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const path = require('path');
 const bodyParser = require('body-parser');
